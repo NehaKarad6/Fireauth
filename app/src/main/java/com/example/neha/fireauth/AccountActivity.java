1598bcda00fbc0014e0dab215894e5d1151762e3
NehaKarad6 <karad.neha6@gmail.com>
2017-04-07 23:53:11 +0530

    normal sign in api

---
+++ app/src/main/java/com/example/neha/fireauth/AccountActivity.java
@@ -0,0 +1,57 @@
+package com.example.neha.fireauth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AccountActivity extends AppCompatActivity {
+    private Button mLogoutbtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+
+
+        mAuth=FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // User is signed out
+                   startActivity(new Intent(AccountActivity.this,MainActivity.class));
+                }
+                // ...
+            }
+        };
+
+
+        mLogoutbtn=(Button)findViewById(R.id.btnlogout);
+        mLogoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+
+
+}
